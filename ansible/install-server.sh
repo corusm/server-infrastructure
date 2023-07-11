a9f90edc0cafcf49354dbc54518f240643952ec9
corusm <dev@corusm.de>
2023-07-11 12:05:18 +0200

    update ansible playbook adding github integration

--- ansible/install-server.sh
+++ ansible/install-server.sh
@@ -3,11 +3,11 @@
 # Check if server.ini is there
 # If not, remove ip from known_hosts
 
-echo "Enter Server IP:"
-read server_ip
+read -p "Enter Server IP: " server_ip
 
-echo -e "[server] \n$server_ip ansible_ssh_user='root'" > server.ini
+echo -e "[server] \n$server_ip ansible_ssh_user=root" > server.ini
 
-ansible-playbook -i server.ini setup-server.yml
+ansible-playbook -i server.ini setup-server.yml --ask-pass -K
 
+# test if connection has worked
 ssh niklas@$server_ip
